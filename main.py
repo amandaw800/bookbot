665306a4f26c8ec978f4a11b58d608c2dd22f7d3
amandaw800 <amanda.v.wei@gmail.com>
2025-08-08 14:00:03 -0400

    Added more to bookbot

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from stats import count_words, get_book_text
+from stats import count_words, get_book_text, character_counter, two_key_val_dict, sort_by_count, print_character_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    total_words = count_words(book_text)
 
+    total_words = count_words(book_text)
+    character_counts = character_counter(book_text)
+
+
+    list_to_sort = two_key_val_dict(character_counts)
+
+    list_to_sort.sort(reverse=True, key=sort_by_count)
+   
+
+    ################################################
+
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(total_words)
+    print("--------- Character Count -------")
+    print_character_count(list_to_sort)
+    print("============= END ===============")
+
+
+
+
 
 
 
--- stats.py
+++ stats.py
@@ -8,8 +8,44 @@ def get_book_text(filepath):
 def count_words(book_text):
     list_of_words = book_text.split()
     word_count = len(list_of_words)
-    return f"{word_count} words found in the document"
+    return f"Found {word_count} total words"
 
-
+#Counts the number of times each character appears in the string
 def character_counter(book_text):
+    counter = {}
+    for char in book_text:
+        if char.isalpha():
+            char = char.lower()
+        if char not in counter:
+            counter[char] = 1
+        else:
+            counter[char] +=1
+    return counter
+
+#Returns dict with char: key, count: value on a list
+def two_key_val_dict(dic):
+    named_key_val = {}
     
+    list_of_dicts = []
+    
+    for key in dic:
+        named_key_val["char"] = key
+        named_key_val["count"] = dic[key]
+        list_of_dicts.append(named_key_val)
+        named_key_val = {}
+    
+    return list_of_dicts
+
+
+def sort_by_count(dict):
+    return dict["count"]
+
+
+def print_character_count(list):
+    for index, value in enumerate(list):
+        current_dict = list[index]
+        current_character = current_dict["char"]
+        if current_character.isalpha():
+            current_count = current_dict["count"]
+
+            print(f"{current_character}: {current_count}")
