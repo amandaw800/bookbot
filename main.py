c67d42ddf11809165ea7359c442922f056e0aeca
amandaw800 <amanda.v.wei@gmail.com>
2025-08-08 14:38:10 -0400

    Added new books and dynamic book reading

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from stats import count_words, get_book_text, character_counter, two_key_val_dict, sort_by_count, print_character_count
+import sys
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+
+
+
+
+    book_text = get_book_text(book_path)
 
     total_words = count_words(book_text)
     character_counts = character_counter(book_text)
@@ -16,7 +26,7 @@ def main():
 
     
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(total_words)
     print("--------- Character Count -------")
